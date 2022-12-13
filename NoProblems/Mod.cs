f30bae156c1241daab478907527f99aa93122f50
MacSergey <macsergey@me.com>
2022-12-13 21:36:43 +0200

    - Required game version changed to 1.16.0-f3

--- NoProblems/Mod.cs
+++ NoProblems/Mod.cs
@@ -30,7 +30,7 @@ namespace NoProblems
             new ModVersion(new Version("2.1"), new DateTime(2022,9,25)),
         };
 
-        protected override Version RequiredGameVersion => new Version(1, 15, 1, 4);
+        protected override Version RequiredGameVersion => new Version(1, 16, 0, 3);
 
 #if BETA
         public override bool IsBeta => true;
